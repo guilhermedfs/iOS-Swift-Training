4cc3df1be644daa3ff1a51f936136598388a28ec
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-11-02 09:53:03 -0300

    New app: Stack view

---
+++ Stack View/Stack View/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Stack View
+//
+//  Created by Guilherme Daniel on 02/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
